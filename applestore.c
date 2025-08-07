1c99c33cfe60d0ff1172a4be997075e3ae6f66b2
forkroyal <gfabits12@gmail.com>
2025-08-07 11:23:09 +0200

    changes in the store 2

--- applestore.c
+++ applestore.c
@@ -2,18 +2,36 @@
 
 int main() {
   
-int appleQuantity;
-float applePrice = 1.49;
-double appleReview;
-int appleReviewDisplay;
-const char appleLocation = 'F';
+ int appleQuantity;
+ double applePrice = 1.49;
 
-appleQuantity = 23;
-appleReview = 82.5;
+ double appleReview;
+ int appleReviewDisplay;
+ const char appleLocation = 'F';
 
-appleReviewDisplay = (int)appleReview;
+ int dayOfWeek = 0;
 
-// Put all your code above this and if you declare your variables using the given names and types there is no need to change any of the code below.
-printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
+ const int appleRevierwers = 9;
+
+ const int totalAppleReviewers = 10;
+
+ appleQuantity = 23;
+
+ appleReview = 823.0 / appleRevierwers; //calculating score
+ appleReviewDisplay = appleReview; //setting scrore to display
+
+ dayOfWeek += 3; //addring 3 days passed
+ 
+ appleReview = (appleReview * appleRevierwers + 52) / totalAppleReviewers; //updating new Reviewscrore
+ appleReviewDisplay = appleReview; //setting updated scrore to display
+
+
+ if(dayOfWeek % 7 == 3 || appleQuantity < 10) //if 3rd day of week or less then 10 apples start sale
+ {
+  printf("Sale on apples today, today only they are: $%.2f\n", applePrice * .9);
+ }
+
+ 
+ printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
 
 }
